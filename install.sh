221986e1b9458ebe12c4aa84b7784f2cd1bcbcf4
orvice <orvice@gmail.com>
2021-08-21 23:37:02 +0800

    install golangci-lint

--- install.sh
+++ install.sh
@@ -5,4 +5,9 @@ curl -sL https://deb.nodesource.com/setup_12.x |  bash - && apt update && apt in
 npm install -g @redocly/openapi-cli redoc-cli
 git clone https://github.com/go-swagger/go-swagger /tmp/go-swagger && cd /tmp/go-swagger && go install ./cmd/swagger
 
+echo "Install reviewdog"
 curl -sfL https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh| sh -s -- -b /usr/bin
+
+echo "Install golangci-lint"
+# binary will be $(go env GOPATH)/bin/golangci-lint
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.42.0
