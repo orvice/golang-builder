5bb72c9fa5be8df04ae660528ec5ea0bca2c151a
orvice <orvice@gmail.com>
2021-08-21 23:49:05 +0800

    Update install path

--- install.sh
+++ install.sh
@@ -8,5 +8,4 @@ echo "Install reviewdog"
 curl -sfL https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh| sh -s -- -b /usr/bin
 
 echo "Install golangci-lint"
-# binary will be $(go env GOPATH)/bin/golangci-lint
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.42.0
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b /usr/bin v1.42.0
