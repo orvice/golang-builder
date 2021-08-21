7520486d118053f45c7aac679040dc57bbdadd1b
orvice <orvice@gmail.com>
2021-08-21 23:39:17 +0800

    remove node

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
-curl -sL https://deb.nodesource.com/setup_12.x |  bash - && apt update && apt install -y nodejs && node --version
-npm install -g @redocly/openapi-cli redoc-cli
+echo "Install go-swagger"
 git clone https://github.com/go-swagger/go-swagger /tmp/go-swagger && cd /tmp/go-swagger && go install ./cmd/swagger
 
 echo "Install reviewdog"
