1bd02584c9afad4d65fa2ba608776018f8ca92f0
orvice <orvice@gmail.com>
2021-08-21 23:35:18 +0800

    Install reviewdog

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ set -ex
 curl -sL https://deb.nodesource.com/setup_12.x |  bash - && apt update && apt install -y nodejs && node --version
 npm install -g @redocly/openapi-cli redoc-cli
 git clone https://github.com/go-swagger/go-swagger /tmp/go-swagger && cd /tmp/go-swagger && go install ./cmd/swagger
+
+curl -sfL https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh| sh -s -- -b /usr/bin
